d5b22d25241f41034225b942a8241396cf18d75c
linzxcw <251114061@qq.com>
2026-09-01 18:06:27 +0800

    LitePan-fpk: auto build x86 + arm fpk packages

---
+++ internal/fusemount/constants.go
@@ -0,0 +1,25 @@
+package fusemount
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	defaultMountRoot     = "/app/mounts"
+	KeyMountRoot         = "fuse_mount_root"
+	KeyEnabled           = "fuse_enabled"
+	DefaultEntryTimeoutS = 30
+	DefaultAttrTimeoutS  = 3
+)
+
+
+var MountRoot = resolveMountRoot()
+
+func resolveMountRoot() string {
+	if v := strings.TrimSpace(os.Getenv("LITEPAN_MOUNT_ROOT")); v != "" {
+		return filepath.Clean(v)
+	}
+	return defaultMountRoot
+}
